5a2b15d40a686028061532869355f3b6448e8127
Karina Ponte <kita_36@hotmail.com>
2020-09-18 10:59:22 -0300

    First Commit

---
+++ javascripts/feed.js
@@ -0,0 +1,19 @@
+async function exibirPosts() {
+  const response = await fetch("https://foodgram-back.herokuapp.com/posts");
+  const arrayDeFotos = await response.json();
+
+  var sectionPosts = document.querySelector("section");
+
+  for (var i = 0; i < arrayDeFotos.length; i++) {
+    sectionPosts.insertAdjacentHTML(
+      "beforeend",
+      `<div class="post">
+          <div>Por: ${arrayDeFotos[i].user}</div>
+          <img src="https://foodgram-back.herokuapp.com/${arrayDeFotos[i].url}" alt="" />
+      </div>
+    `
+    );
+  }
+}
+
+exibirPosts();
---
+++ javascripts/login.js
@@ -0,0 +1,14 @@
+var btnLogin = document.querySelector("#btnLogin");
+
+function login() {
+  const inputNome = document.querySelector("#inputNome");
+
+  if (inputNome.value.length === 0) {
+    return alert("Digite um nome primeiro");
+  }
+  sessionStorage.setItem("nomeUsuario", inputNome.value);
+
+  location.href = "/feed.html";
+}
+
+btnLogin.addEventListener("click", login);
